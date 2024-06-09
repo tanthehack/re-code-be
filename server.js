de475bc50e2ac369d479a27642efea2fe2c4f3a4
tanthehack <31630356+tanthehack@users.noreply.github.com>
2024-06-09 20:48:58 +0100

    add: import code endpoint

--- server.js
+++ server.js
@@ -72,14 +72,7 @@ app.get('/getAccessToken', async function (req, res) {
     });
 });
 
-//page and limit
 app.get('/getUserRepos', async function (req, res) {
-    // const page = req.query.page;
-    // const limit = req.query.limit;
-
-    // const startIndex = (page - 1) * limit;
-    // const endIndex = page * limit
-
     const authHeader = req.get("Authorization");
     if (!authHeader) {
         return res.status(400).json({ error: "Authorization header is required" });
@@ -110,6 +103,66 @@ app.get('/getUserRepos', async function (req, res) {
 
         const data = await response.json();
         console.log("the data", data);
+        res.json(data);
+    } catch (error) {
+        console.error("Error fetching data from GitHub API:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+})
+
+app.post('/importCode', async function (req, res) {
+    const authHeader = req.get("Authorization");
+    if (!authHeader) {
+        return res.status(400).json({ error: "Authorization header is required" });
+    }
+
+    const token = getToken(authHeader);
+    if (!token) {
+        return res.status(401).json({ error: "Invalid token" });
+    }
+
+    const body = req.body;
+    const { repo, owner } = body;
+
+    if (!repo || !owner) {
+        return res.status(400).json({ error: "The repo name and owner name are required!" });
+    }
+
+    try {
+        const response = await fetch(`https://api.github.com/repos/${owner}/${repo}/languages`, {
+            method: "GET",
+            headers: {
+                "Accept": "application/vnd.github+json",
+                "Authorization": `Bearer ${token}`,
+                "X-GitHub-Api-Version": "2022-11-28"
+            }
+        })
+
+        if (response.status === 401) {
+            console.error("Unauthorized: Invalid or expired token");
+            return res.status(401).json({ error: "Unauthorized: Invalid or expired token" });
+        } else if (response.status === 403) {
+            console.error("Forbidden: Access denied");
+            return res.status(403).json({ error: "Forbidden: Access denied" });
+        } else if (!response.ok) {
+            console.error(`Error: ${response.statusText}`);
+            return res.status(response.status).json({ error: `Error: ${response.statusText}` });
+        }
+
+        let data;
+        try {
+            data = await response.json();
+
+        } catch (jsonError) {
+            console.error("Invalid JSON response", jsonError);
+            return res.status(502).json({ error: "Bad Gateway: Invalid JSON response from API" });
+        }
+
+        if (!data.JavaScript) {
+            console.error("JavaScript code not found in the repository");
+            return res.status(400).json({ error: "JavaScript code not found in the repository" });
+        }
+
         res.json(data);
     } catch (error) {
         console.error("Error fetching data from GitHub API:", error);
